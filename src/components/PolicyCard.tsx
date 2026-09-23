0eb1796c2b779110008be19479f84b0c8b399443
David Kågell <David.kagell@hotmail.com>
2026-09-23 11:41:03 +0200

    making PolicyCard pretty on all devices

--- src/components/PolicyCard.tsx
+++ src/components/PolicyCard.tsx
@@ -12,28 +12,34 @@ export function PolicyCard({
   const container = "p-2 flex flex-col items-start";
 
   return (
-    <section className="flex flex-col border-2 border-blue-200 rounded-xl overflow-hidden">
-      <div className={`flex flex-col bg-blue-100 ${container}`}>
-        {policyStatus === "Inactive" && <p>Din försäkring har avslutats</p>}
+    <section className=" mx-4 flex flex-col border-2 border-blue-200 rounded-xl overflow-hidden">
+      <div
+        className={`flex flex-col bg-blue-100 ${container} px-0! border-b border-b-blue-200`}
+      >
+        {policyStatus === "Inactive" && (
+          <p className="bg-red-100 text-red-800 rounded-full m-2 px-2 border border-red-500">
+            Din försäkring har avslutats
+          </p>
+        )}
         <ul className={`${container}`}>
-          <li className="font-extrabold">{productName}</li>
+          <li className="font-bold text-xl">{productName}</li>
           <li>{policyDescription}</li>
         </ul>
       </div>
       <ul
         className={`${container} w-full gap-0 divide-y divide-blue-200 items-stretch`}
       >
-        <li className="w-full py-1 flex after:flex-1 after:content-['']">
-          <p className="flex-1 text-left">Startdatum:</p>
-          <p className="flex-1 text-center">{policyStartDate}</p>
+        <li className="grid w-full grid-cols-2 items-center py-1 truncate">
+          <p className="font-bold text-left truncate">Startdatum:</p>
+          <p className="text-left">{policyStartDate}</p>
         </li>
-        <li className="w-full py-1 flex after:flex-1 after:content-['']">
-          <p className="flex-1 text-left">Försäkringsnummer:</p>
-          <p className="flex-1 text-center">{policyNumber}</p>
+        <li className="grid w-full grid-cols-2 items-center py-1">
+          <p className="font-bold text-left truncate">Försäkringsnummer:</p>
+          <p className="text-left ">{policyNumber}</p>
         </li>
-        <li className="w-full py-1 flex after:flex-1 after:content-['']">
-          <p className="flex-1 text-left">Pris per månad:</p>
-          <p className="flex-1 text-center">{monthlyPrice} kr</p>
+        <li className="grid w-full grid-cols-2 items-center py-1">
+          <p className="font-bold text-left truncate">Pris per månad:</p>
+          <p className="text-left">{monthlyPrice} kr</p>
         </li>
       </ul>
     </section>
