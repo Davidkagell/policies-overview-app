395cca878972c3fb17130af62a11d55fb81d1168
David Kågell <David.kagell@hotmail.com>
2026-09-23 10:46:17 +0200

    add PolicyList with props and mapping data

---
+++ src/components/PolicyList.tsx
@@ -0,0 +1,16 @@
+import { PolicyCard } from "./PolicyCard";
+import type { Policy } from "../data/types";
+
+type PolicyListProps = {
+  policies: Policy[];
+};
+
+export function PolicyList({ policies }: PolicyListProps) {
+  return (
+    <div className="flex flex-col gap-4">
+      {policies.map((policy) => (
+        <PolicyCard key={policy.policyNumber} {...policy} />
+      ))}
+    </div>
+  );
+}
