87561697942b5de0ddfc207764ac48d2a6f5280d
David Kågell <David.kagell@hotmail.com>
2026-09-24 11:49:14 +0200

    add mapping to PolicyList

--- src/components/PolicyList.tsx
+++ src/components/PolicyList.tsx
@@ -7,10 +7,12 @@ type PolicyListProps = {
 
 export function PolicyList({ policies }: PolicyListProps) {
   return (
-    <div className="flex flex-col gap-4">
-      {policies.map((policy) => (
-        <PolicyCard key={policy.policyNumber} {...policy} />
-      ))}
-    </div>
+    <>
+      <div className="flex flex-col gap-4 ">
+        {policies.map((policy) => (
+          <PolicyCard key={policy.policyNumber} {...policy} />
+        ))}
+      </div>
+    </>
   );
 }
